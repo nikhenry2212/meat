1fd6c90254bfd2906076060b4f9478da6b93b89f
Nikolas <nikolaspg@hotmail.com>
2021-04-14 14:24:52 -0300

    tratandoErroComOperadorCatch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,3 +1,4 @@
+
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 
@@ -7,41 +8,24 @@ import { Restaurant } from "./restaurant/restaurant.model";
 
 import 'rxjs/add/operator/map';
 // import pra usar modelo de tipagem da classe restaurant
+import 'rxjs/add/operator/catch';
+//import de tratamento de erro
 
 import {MEAT_API} from './../app.api';
+import { ErrorHandler} from '../app.error-handler'
+//import da class q o erro foi tratado
 
 @Injectable()
 export class RestaurantsService {
 
-  //rest: Restaurant[] = [
-
-    // //trouxe a simulação do back-end para o service
-
-    // //simulando dados do back-end
-    //     {
-    //       id: "bread-bakery",
-    //       name: "Bread & Bakery",
-    //       category: "Bakery",
-    //       deliveryEstimate: "25m",
-    //       rating: 4.9,
-    //       imagePath: "assets/img/restaurants/breadbakery.png",
-
-    //     },
-    //     {
-    //       id: "burger-house",
-    //       name: "Burger House",
-    //       category: "Hamburgers",
-    //       deliveryEstimate: "100m",
-    //       rating: 3.5,
-    //       imagePath: "assets/img/restaurants/burgerhouse.png",
-
-    //     }
-    // ]
 
   constructor(private http: Http){}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json());
+    return this.http.get(`${MEAT_API}/restaurants`)
+    // return this.http.get(`${MEAT_API}/restaurants1`) teste de erro
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 
 
