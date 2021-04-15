bf3299f7a4c0b1b2f4b285fb8909181bbf143311
Nikolas <nikolaspg@hotmail.com>
2021-04-15 12:20:23 -0300

    🚀: Criando rotas filhas menu e avaliações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'restaurants', component: RestaurantsComponent},
-  {path: 'restaurants/:id', component: RestaurantDetailComponent},//coloquei uma rota e sinalize que irá receber um argumento
-  {path: 'about', component: AboutComponent}
+  { path: '', component: HomeComponent },
+  { path: 'restaurants', component: RestaurantsComponent },
+  //coloquei uma rota e sinalize que irá receber um argumento
+  {
+    path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      //navegação filhas para dois components menu e avaliações
+      { path: '', redirectTo: 'menu', pathMatch: 'full' },
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviews', component: ReviewsComponent }
+    ]
+  },
+
+  { path: 'about', component: AboutComponent }
   // add rota para component
 ]
