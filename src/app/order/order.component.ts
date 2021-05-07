8929d7054916bb25da31b70091a62d87577784d1
Nikolas <nikolaspg@hotmail.com>
2021-05-07 16:14:28 -0300

    Finalizando A Compra HTTP POST 💻

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { Order, OrderItem } from './order.model';
 import { OrderService } from './order.service';
 
 @Component({
@@ -38,6 +39,14 @@ itemsValue(): number{
   }
   remove(item: CartItem){
     this.orderService.remove(item);
+  }
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe(( orderId: string) => {
+      console.log(`Compra concluida: ${orderId}`)
+      this.orderService.clear()
+    })
+    console.log(order);
   }
 
 }
