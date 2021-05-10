842a34ceeab83a9f48d980a870cf2f7be76c795d
Nikolas <nikolaspg@hotmail.com>
2021-05-10 14:59:20 -0300

    Criando Um Modulo Compartilhado  🏗

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,13 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
+
 import { OrderItemsComponent } from './order/order-items/order-items.component'; //importei o modulo pq estava dando erro no providers
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import { SharedModule } from './shared/shared.module'; ///modulo compartilhado (input,radio,raiting)
+
 //importei o modulo
 
 //import do component Restaurant
@@ -45,18 +45,15 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent,
+
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,// declara NO IMPORTS
-    ReactiveFormsModule,
+    SharedModule,//declaração
     RouterModule.forRoot(ROUTES)// modulo about foi compartilhado
 
   ],//LOCALE_ID provide para mudar o valor da moeda
