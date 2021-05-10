107fc32de99a413d66602442fc9584efb39f5cc3
Nikolas <nikolaspg@hotmail.com>
2021-05-10 12:05:48 -0300

    Criando Validadores Personalizados 🔓✔

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { Order, OrderItem } from './order.model';
 import { OrderService } from './order.service';
 
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -18,54 +18,68 @@ export class OrderComponent implements OnInit {
   //expressão de email e numeros
   numberPattern = /^[0-9]*$/
 
-    orderForm: FormGroup;
-    //referencia de FormGroup
+  orderForm: FormGroup;
+  //referencia de FormGroup
 
-    delivery: number = 8;
-    //Valor fixo do frete
+  delivery: number = 8;
+  //Valor fixo do frete
 
   paymentOptions: RadioOption[] = [
-    {label: "Dinheiro", value: "MON"},
-    {label: "Cartão de Débito", value: "DEB"},
-    {label: "Cartão Refeição", value: "REF"}
+    { label: "Dinheiro", value: "MON" },
+    { label: "Cartão de Débito", value: "DEB" },
+    { label: "Cartão Refeição", value: "REF" }
   ]
 
-  constructor (
-              private orderService: OrderService,
-              private router: Router,
-              private formBuilder: FormBuilder
-              ) { }
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),//Validators substituindo o ngModel
-      email: this.formBuilder.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
-      emailConfirmation: this.formBuilder.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
-      address:this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),
-      number: this.formBuilder.control('', [Validators.required,Validators.pattern(this.numberPattern)]),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),//Validators substituindo o ngModel
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('',[Validators.required]),
-    })
+      paymentOption: this.formBuilder.control('', [Validators.required]),
+    }, { validator: OrderComponent.equalsTo })
   }
-itemsValue(): number{
-  return this.orderService.itemsValue();
-}
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true }
+    }
+    return undefined
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
+  }
 
   cartItems(): CartItem[] {
     return this.orderService.cartItems();
   }
-  increaseQty(item: CartItem){
+  increaseQty(item: CartItem) {
     this.orderService.increaseQty(item);
   }
-  decreaseQty(item: CartItem){
+  decreaseQty(item: CartItem) {
     this.orderService.decreaseQty(item);
   }
-  remove(item: CartItem){
+  remove(item: CartItem) {
     this.orderService.remove(item);
   }
-  checkOrder(order: Order){
+  checkOrder(order: Order) {
     order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
-    this.orderService.checkOrder(order).subscribe(( orderId: string) => {
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
       this.router.navigate(['/order-summary'])// criei uma rota usando Router e usando metodo navigate passando a rota
       this.orderService.clear()
     })
