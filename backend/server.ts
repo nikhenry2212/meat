7d548bdf2024676f5f6c2fc1244b9b25d9011c9c
Nikolas <nikolaspg@hotmail.com>
2021-06-11 14:36:02 -0300

    teste1

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
-import * as jsonServer from 'json-server';
-import { Express } from 'express';
 
-import * as fs from 'fs';
-import * as https from 'https';
-import express = require('express');
+import * as jsonServer from 'json-server'
+import { Express } from 'express'
+
+import * as fs from 'fs'
+import * as https from 'https'
+
+import {handleAuthentication} from './auth';
+// import {handleAuthorization} from './authz'
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -11,13 +14,13 @@ const middlewares = jsonServer.defaults()
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-
-
-
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
+//middleware para login
+server.post('/login', handleAuthentication)
+// server.use('/orders', handleAuthorization)
 
 // Use default router
 server.use(router)
@@ -27,10 +30,6 @@ const options = {
   key: fs.readFileSync('./backend/keys/key.pem')
 }
 
-const urlHttps = 'https://localhost:3001'
-https.createServer(options, server).listen(3001, () => {
-  console.log(`JSON Server is running on ${urlHttps}`)
+https.createServer(options, server).listen(3002, () => {
+  console.log('JSON Server is running on https://localhost:3002')
 })
-
-
-
