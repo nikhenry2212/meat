ce25f0a4a5432b86609817c112299de5930e3e34
nikolas <nikolaspg74@gmail.com>
2021-12-20 14:33:46 -0300

    inserção de token jwt no código

--- backend/dist/auth.js
+++ backend/dist/auth.js
@@ -2,11 +2,13 @@
 exports.__esModule = true;
 exports.handleAuthentication = void 0;
 var users_1 = require("./users");
+var jwt = require("jsonwebtoken");
 var handleAuthentication = function (req, resp) {
     var user = req.body;
     if (isValid(user)) {
         var dbUser = users_1.users[user.email];
-        resp.json({ name: dbUser.name, email: dbUser.email });
+        var token = jwt.sign({ sub: dbUser.email, iss: "meat-api" }, 'meat-api-password');
+        resp.json({ name: dbUser.name, email: dbUser.email, acessTokem: token });
     }
     else {
         resp.status(403).json({ message: 'Dados inválidos' });
